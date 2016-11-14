e4d2654190de48f972fc33fddf9f010ad41b1700
Liliya Lantukh <liliya.lantukh@gmail.com>
2016-11-14 18:56:51 -0500

    deleted a migration bc info was wrong, replaced it with a new one

--- database/migrations/1479164724802_comments.js
+++ database/migrations/1479164724802_comments.js
@@ -8,8 +8,8 @@ class CommentsTableSchema extends Schema {
     this.create('comments', (table) => {
       table.increments()
       table.timestamps()
-      table.integer('post_id')
-      table.foreign('post_id').references('posts.id')
+      table.integer('link_id')
+      table.foreign('link_id').references('links.id')
       table.integer('user_id')
       table.foreign('user_id').references('users.id')
       table.string('content')
